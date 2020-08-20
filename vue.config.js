2d8565e57728839e828fa53b71c7525ca3b681bb
aaron_liu <aaron.liu@belstar.com.cn>
2020-08-20 09:05:11 +0800

    webpack优化，删除冗余文件

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 module.exports = {
-  publicPath:'./',
+  publicPath: './',
+  productionSourceMap: false,// 是否需要生产环境的 source map
+  css: {
+    sourceMap: false, // 是否为 CSS 开启 source map
+  },
   devServer: {
     disableHostCheck: true,//webpack4.0 开启热更新
     proxy: {
@@ -12,6 +20,41 @@ module.exports = {
         }
       }
     }
+  },
+  configureWebpack: {
+    mode: 'production',
+    externals: {
+      echarts: 'echarts'
+    },
+    module: {
+      rules: [
+        {
+          //js文件才使用babel
+          test: /\.js$/,
+          loader: 'babel-loader',
+          //只在src文件夹下查找
+          include: [path.resolve(__dirname, 'src')],
+          //不会去查找的路径
+          exclude: /node_modules/
+        }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name][hash].css',
+        chunkFilename: '[id][hash].css'
+      })
+    ],
+    optimization: {
+      minimizer: [
+        new UglifyJsPlugin({
+          cache: true,
+          parallel: true,
+          sourceMap: false // set to true if you want JS source maps
+        }),
+        new OptimizeCSSAssetsPlugin({})
+      ]
+    },
   },
   chainWebpack: config => {
     // 如果使用多页面打包，使用vue inspect --plugins查看html是否在结果数组中
